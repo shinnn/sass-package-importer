a1d413614bc3ef025060d23f60e343d1cb24418f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-07-26 12:33:17 +0900

    make use of object shorthand and destructuring assignment
    [BREAKING] drop support for Node.js < 6

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 'use strict';
 
-const extname = require('path').extname;
-const readFile = require('fs').readFile;
+const {extname} = require('path');
+const {readFile} = require('fs');
 
 module.exports = function sassPackageImporter(path, prev, done) {
+	let resolvedPath;
+
 	try {
-		const resolvedPath = require.resolve(path);
-		const ext = extname(resolvedPath).slice(1).toLowerCase();
-
-		if (ext === 'js' || ext === 'json' || ext === 'mjs') {
-			done({file: path});
-			return;
-		}
-
-		readFile(resolvedPath, 'utf8', (err, contents) => done(err || {
-			file: resolvedPath,
-			contents: contents // eslint-disable-line object-shorthand
-		}));
+		resolvedPath = require.resolve(path);
 	} catch (err) {
 		done({file: path});
+		return;
 	}
+
+	const ext = extname(resolvedPath).slice(1).toLowerCase();
+
+	if (ext === 'js' || ext === 'json' || ext === 'mjs') {
+		done({file: path});
+		return;
+	}
+
+	readFile(resolvedPath, 'utf8', (err, contents) => done(err || {file: resolvedPath, contents}));
 };
